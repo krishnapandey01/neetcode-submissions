61cb21ec99a0819bcb47c928b5b79830393df440
krishnapandey01 <87866990+krishnapandey01@users.noreply.github.com>
2026-05-31 23:15:57 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> left, right;
+        for (auto x : nums) {
+            int l = left.count(x) ? left[x] : x;
+            int r = right.count(x) ? right[x] : x;
+            if (left.count(x - 1)) {
+                l = left[x - 1];
+                left.erase(x - 1);
+            }
+            if (right.count(x + 1)) {
+                r = right[x + 1];
+                right.erase(x + 1);
+            }
+            left[r] = l;
+            right[l] = r;
+        }
+
+        int max_len = 0;
+        for (auto [l, r] : right) {
+            max_len = max(max_len, r - l + 1);
+        }
+        return max_len;
+    }
+};
