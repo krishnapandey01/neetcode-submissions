451b8314641d19f349ca093d79ac163d5bc0f692
krishnapandey01 <87866990+krishnapandey01@users.noreply.github.com>
2026-06-29 01:01:12 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    int binary_search(int l, int r, vector<int>&nums, int target)
+    {
+        int mid = l+ (r-l)/2;
+
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+
+        if(l>r)
+        return -1;
+
+        if(nums[mid]>target)
+        {
+            r = mid-1;
+        }
+        else
+        {
+            l = mid+1;
+        }
+
+
+        return binary_search(l, r, nums, target);
+        
+    }
+
+
+    int search(vector<int>& nums, int target) {
+
+        return binary_search(0, nums.size() - 1, nums, target);
+                
+    }
+};
