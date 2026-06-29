a2d33c6ca47e80c326e2b9bd3f6b42f81fd68d95
krishnapandey01 <87866990+krishnapandey01@users.noreply.github.com>
2026-06-29 13:12:24 +0530

    Add: search-2d-matrix - submission-11

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-11.cpp
@@ -0,0 +1,46 @@
+class Solution {
+   public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size(), col = matrix[0].size();
+        int l = 0, r = rows - 1, res, mid;
+        mid = l + (r - l) / 2;
+
+        if (rows == 1 && col == 1) {
+            if (matrix[0][0] == target)
+                return true;
+            else
+                return false;
+        }
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            cout << mid << "df ";
+            if (target >= matrix[mid][0] && target <= matrix[mid][col - 1]) {
+                res = mid;
+                break;
+            }
+            if (matrix[mid][col - 1] < target)
+                l = mid + 1;
+            else
+                r = mid-1;
+        }
+
+        if (l > r)
+            return false;
+        else
+            res = mid;
+
+        l = 0;
+        r = col - 1;
+
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (matrix[res][mid] == target) return true;
+            if (matrix[res][mid] < target)
+                l = mid + 1;
+            else
+                r = mid-1;
+        }
+
+        return false;
+    }
+};
